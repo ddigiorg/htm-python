505d6d60d450dd4c06163c880a0009e512359185
David Di Giorgio <ddigiorg@gmail.com>
2016-04-07 14:25:12 -0700

    modified cube and main

--- cube.py
+++ cube.py
@@ -2,78 +2,78 @@ from OpenGL.GL import *
 from OpenGL.GLU import *
 
 color_dict = {
-    'r': (1,0,0),
-    'g': (0,1,0),
-    'b': (0,0,1),
-    'w': (1,1,1)
-    }
+	'r': (1,0,0),
+	'g': (0,1,0),
+	'b': (0,0,1),
+	'w': (1,1,1)
+	}
 
 class Cube:
-    def __init__(self):    
-        self.position = (0,0,0)
-        
-        self.color = color_dict['w']
-        
-        self.vertices = (
-            ( 1, -1, -1),
-            ( 1,  1, -1),
-            (-1,  1, -1),
-            (-1, -1, -1),
-            ( 1, -1,  1),
-            ( 1,  1,  1),
-            (-1, -1,  1),
-            (-1,  1,  1)
-            )
+	def __init__(self):    
+		self.position = (0, 0, 0)
+      
+		self.vertex_positions = [  1.0,  1.0,  1.0,  1.0,  1.0, -1.0, -1.0,  1.0, -1.0, 
+								   1.0,  1.0,  1.0, -1.0,  1.0, -1.0, -1.0,  1.0,  1.0,
+								  -1.0,  1.0,  1.0, -1.0, -1.0,  1.0,  1.0, -1.0,  1.0, 
+								   1.0,  1.0,  1.0, -1.0,  1.0,  1.0,  1.0, -1.0,  1.0, 
+								  -1.0, -1.0, -1.0, -1.0, -1.0,  1.0, -1.0,  1.0,  1.0, 
+								  -1.0, -1.0, -1.0, -1.0,  1.0,  1.0, -1.0,  1.0, -1.0, 
+								   1.0,  1.0, -1.0, -1.0, -1.0, -1.0, -1.0,  1.0, -1.0, 
+								   1.0,  1.0, -1.0,  1.0, -1.0, -1.0, -1.0, -1.0, -1.0, 
+								   1.0,  1.0,  1.0,  1.0, -1.0, -1.0,  1.0,  1.0, -1.0, 
+								   1.0, -1.0, -1.0,  1.0,  1.0,  1.0,  1.0, -1.0,  1.0, 
+								   1.0, -1.0,  1.0, -1.0, -1.0, -1.0,  1.0, -1.0, -1.0, 
+								   1.0, -1.0,  1.0, -1.0, -1.0,  1.0, -1.0, -1.0, -1.0] 
 
-        self.edges = (
-            (0,1),
-            (0,3),
-            (0,4),
-            (2,1),
-            (2,3),
-            (2,7),
-            (6,3),
-            (6,4),
-            (6,7),
-            (5,1),
-            (5,4),
-            (5,7)    
-                )
-
-        self.surfaces = (
-            (0,1,2,3),
-            (3,2,7,6),
-            (6,7,5,4),
-            (4,5,1,0),
-            (1,5,7,2),
-            (4,0,3,6)
-            )
-
-    def setPosition(self, position):
-        self.position = position
-        new_vertices = []
-        for vertex in self.vertices:
-            new_vertices.append( (
-                vertex[0] + position[0],
-                vertex[1] + position[1],
-                vertex[2] + position[2]
-                ) )
-
-        self.vertices = new_vertices
-
-    def setColor(self, color):
-        self.color = color_dict[color]
-
-    def getPosition(self):
-        return self.position
-
-    def getColor(self):
-        return self.color
-
-    def getVertices(self):
-        return self.vertices
-
-    def getSurfaces(self):
-        return self.surfaces
+		self.color = color_dict['w']
+      
+		self.vertex_colors    = [  1.0,  1.0,  1.0,  1.0,  1.0,  1.0,  1.0,  1.0,  1.0,
+								   1.0,  1.0,  1.0,  1.0,  1.0,  1.0,  1.0,  1.0,  1.0,
+								   0.8,  0.8,  0.8,  0.8,  0.8,  0.8,  0.8,  0.8,  0.8,
+								   0.8,  0.8,  0.8,  0.8,  0.8,  0.8,  0.8,  0.8,  0.8,
+								   0.6,  0.6,  0.6,  0.6,  0.6,  0.6,  0.6,  0.6,  0.6,
+								   0.6,  0.6,  0.6,  0.6,  0.6,  0.6,  0.6,  0.6,  0.6,
+								   0.4,  0.4,  0.4,  0.4,  0.4,  0.4,  0.4,  0.4,  0.4,
+								   0.4,  0.4,  0.4,  0.4,  0.4,  0.4,  0.4,  0.4,  0.4,
+								   0.2,  0.2,  0.2,  0.2,  0.2,  0.2,  0.2,  0.2,  0.2,
+								   0.2,  0.2,  0.2,  0.2,  0.2,  0.2,  0.2,  0.2,  0.2,
+								   0.1,  0.1,  0.1,  0.1,  0.1,  0.1,  0.1,  0.1,  0.1,
+								   0.1,  0.1,  0.1,  0.1,  0.1,  0.1,  0.1,  0.1,  0.1]
 
 
+	def setPosition(self, x, y, z):
+		self.position = (x, y, z)
+		new_vertex_positions = []
+		for i in range(0, len(self.vertex_positions), 3):
+			new_vertex_positions.append(self.vertex_positions[i  ] + x)
+			new_vertex_positions.append(self.vertex_positions[i+1] + y)
+			new_vertex_positions.append(self.vertex_positions[i+2] + z)
+
+		self.vertex_positions = new_vertex_positions
+
+
+	def setColor(self, color):
+		self.color = color_dict[color]
+		new_vertex_colors = []
+		for i in range(0, len(self.vertex_colors), 3):
+			new_vertex_colors.append(0.0)
+			new_vertex_colors.append(0.0)
+			new_vertex_colors.append(1.0)
+
+		self.vertex_colors = new_vertex_colors
+
+
+	def getPosition(self):
+		return self.position
+
+
+	def getColor(self):
+		return self.color
+
+
+	def getVertexPositions(self):
+		return self.vertex_positions
+
+
+	def getVertexColors(self):
+		return self.vertex_colors
