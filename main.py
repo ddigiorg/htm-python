42924819bfd5f274d8124fc00c6cf2fe4c93510b
David Di Giorgio <ddigiorg@gmail.com>
2016-06-05 13:47:40 -0700

    updated code

--- main.py
+++ main.py
@@ -1,26 +1,29 @@
-import layer as lay
+import cortical_layer as cl
 import spatial_pooler as sp
+import temporal_memory as tm
+
 
 inputs = [0] * 10
 inputs[0] = 1
 inputs[1] = 1
+inputs[2] = 1
 
-layer = lay.Layer(len(inputs), 3, 2)
+layer = cl.CorticalLayer(
+	num_inputs = len(inputs),
+	num_columns = 3,
+	num_neurons = 2)
 
-(neurons_active_addresses,
-neurons_predict_addresses,
+(n_active_states,
+n_predict_states,
+n_learn_states,
 s_threshold,
 s_learning_rate,
 s_permanence_lower,
 s_permanence_upper,
 ps_addresses,
-ps_permanances,
+ps_permanences,
 bs_addresses,
-bs_permanances) = layer.getLayer()
-
-print(inputs)
-print(ps_addresses)
-print(ps_permanances)
+bs_permanences) = layer.getLayer()
 
 spool = sp.SpatialPooler(
 	inputs,
@@ -29,8 +32,27 @@ spool = sp.SpatialPooler(
 	s_permanence_lower,
 	s_permanence_upper,
 	ps_addresses,
-	ps_permanances)
+	ps_permanences)
+
+tmem = tm.TemporalMemory(
+	n_active_states,
+	n_predict_states,
+	n_learn_states,
+	s_threshold,
+	s_learning_rate,
+	s_permanence_lower,
+	s_permanence_upper,
+	bs_addresses,
+	bs_permanences)
+
+#print(inputs)
+#print(ps_addresses)
+#print(ps_permanances)
+
+#for i in range(100):
+spool.run()
+tmem.run(spool.getActiveColumnsAddresses())
+tmem.run(spool.getActiveColumnsAddresses())
 
 
-for i in range(100):
-	spool.run()
+#print(ps_permanences)
--- spatial_pooler.py
+++ spatial_pooler.py
@@ -4,43 +4,51 @@ class SpatialPooler(object):
 
 	def __init__(self, inputs, s_threshold, s_learning_rate, s_permanence_lower, s_permanence_upper, ps_addresses, ps_permanences):
 	
+		# Inputs
 		self.inputs = inputs
+
+		# Synapse Variables
 		self.s_threshold = s_threshold
 		self.s_learning_rate = s_learning_rate
 		self.s_permanence_lower = s_permanence_lower
 		self.s_permanence_upper = s_permanence_upper
+
+		# Proximal Synapse Data Variables
 		self.ps_addresses = ps_addresses
 		self.ps_permanences = ps_permanences
 
-		self.n_inputs = len(inputs)
-		self.n_columns = len(ps_addresses)
-		self.n_synapses = len(ps_addresses[0])
+		# Other Variables
+		self.num_inputs = len(inputs)
+		self.num_columns = len(ps_addresses)
+		self.num_synapses = len(ps_addresses[0])
 
+		# Active Column Addresses for Temporal Memory
+		self.ac_addresses = np.zeros(1, dtype=np.int8)
 		self.active_columns_percent = 0.2
-		self.n_active_columns = np.int16(np.ceil(self.n_columns * self.active_columns_percent))
-		self.active_columns_addresses = [0] * self.n_active_columns
+		self.num_active_columns = np.int16(np.ceil(self.num_columns * self.active_columns_percent))
 
 	def run(self):
+
+		# ADD BOOSTING TO SPATIAL POOLER
 		
 		# Overlap
-		overlap = [0] * self.n_columns
-		for c in range(self.n_columns):
-			for s in range(self.n_synapses):
+		overlap = [0] * self.num_columns
+		for c in range(self.num_columns):
+			for s in range(self.num_synapses):
 				if self.ps_permanences[c, s] > self.s_threshold:
 					overlap[c] = overlap[c] + self.inputs[self.ps_addresses[c, s]]
 
-#		print(overlap)
-
 		# Inhibiion
-		for ac in range(self.n_active_columns):
-			self.active_columns_addresses[ac] = np.argmax(overlap)
+		self.ac_addresses = np.zeros(self.num_active_columns, dtype=np.int8)
+		for ac in range(self.num_active_columns):
+			self.ac_addresses[ac] = np.argmax(overlap)
 			overlap[ac] = 0
-	
-#		print(active_columns_addresses)
 
-		# Learning
-		for ac in self.active_columns_addresses:
-			for s in range(self.n_synapses):
+		print(self.ac_addresses)
+	
+		# Learning (MAYBE LOOK INTO MAKING LEARNING ITS OWN FUNCTION IN LAYER CLASS?)
+		for ac in self.ac_addresses:
+			for s in range(self.num_synapses):
 				address = self.ps_addresses[ac, s]
 				input_value = self.inputs[address]
 				permanence = self.ps_permanences[ac, s]
@@ -49,15 +57,13 @@ class SpatialPooler(object):
 				elif input_value == 0 and permanence > self.s_permanence_lower:
 					self.ps_permanences[ac, s] -= self.s_learning_rate
 				elif permanence == 0:
-					connections = np.zeros(self.n_inputs, dtype=np.int8)
+					connections = np.zeros(self.num_inputs, dtype=np.int8)
 					connections[self.ps_addresses[ac]] = 1
 					new_connections = np.logical_not(connections)
-					new_addresses = np.nonzero(new_connections > 0)[0] #why the [0]???? 
+					new_addresses = np.nonzero(new_connections > 0) #why the [0]???? 
 		
 					self.ps_addresses[ac, s] = np.random.choice(new_addresses, None, replace=False)
 					self.ps_permanences[ac, s] = 21
 
-		print(self.ps_permanences)
-
-		def get_active_columns_addresses(self):
-			return self.active_columns_addresses
+	def getActiveColumnsAddresses(self):
+		return self.ac_addresses
