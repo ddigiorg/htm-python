ddfe8056d8c53ab370d046de7b14f32d9f51396f
David Di Giorgio <ddigiorg@gmail.com>
2016-08-07 05:43:02 -0700

    modified htm

--- htm.py
+++ htm.py
@@ -2,6 +2,7 @@ import numpy as np
 
 # c means column
 # n means neuron
+# d means dendrite segment
 # s means synapse
 # ps means proximal synapse
 # bs means basal synapse
@@ -32,17 +33,28 @@ class Layer3b(object):
 		self.ps_permanences = np.random.random_integers(self.s_threshold, self.s_threshold + 1, (num_columns, self.num_psynapses) )
 
 		# L3b basal synapse data variables
-		self.bs_addresses   = np.full( (num_columns, num_neurons), None, dtype=object)
-		self.bs_permanences = np.full( (num_columns, num_neurons), None, dtype=object)
+		self.bs_addresses   = np.empty([num_columns, num_neurons], dtype=object)
+		self.bs_permanences = np.empty([num_columns, num_neurons], dtype=object)
+
+		print(np.array(self.bs_addresses))
 
 	def runSpatialPooler(self, inputs):
 		self.c_active_addresses = SpatialPooler(inputs, self.ps_addresses, self.ps_permanences, self.s_threshold)
 
+#		print(self.ps_permanences[self.c_active_addresses])
+
 	def runTemporalMemory(self):
-		self.n_active_addresses = TemporalMemory(self.num_neurons, 
+		self.n_active_addresses = TemporalMemory(self.num_columns,
+                                                 self.num_neurons, 
+                                                 self.bs_addresses,
+                                                 self.bs_permanences,
                                                  self.c_active_addresses, 
                                                  self.n_active_addresses, 
-                                                 self.n_predict_addresses)
+                                                 self.n_predict_addresses,
+                                                 self.n_learn_addresses)
+
+#		print(self.n_active_addresses)
+
 
 	def getActiveColumnAddresses(self):
 		return self.c_active_addresses
@@ -79,55 +91,89 @@ def SpatialPooler(input_axons, ps_addresses, ps_permanences, s_threshold):
 
 	# Learning
 	for ac in c_active_addresses:
-		adaptSynapses(input_axons, ps_addresses[ac], ps_permanences[ac], s_threshold)
+		adaptSynapses(input_axons, [ps_addresses[ac]], [ps_permanences[ac]], s_threshold)
 
 	return c_active_addresses
 
 
-def TemporalMemory(num_neurons, c_active_addresses, n_active_addresses, n_predict_addresses):
+def TemporalMemory(num_columns,
+                   num_neurons,
+                   bs_addresses,
+                   bs_permanences,
+                   c_active_addresses,
+                   n_active_addresses,
+                   n_predict_addresses,
+                   n_learn_addresses):
 
-	n_active_addresses = []
+	n_previous_active_addresses  = n_active_addresses
+	n_previous_predict_addresses = n_predict_addresses
+	n_previous_learn_addresses   = n_learn_addresses
 
-	# Determine neuron axons' active state
+	n_active_addresses  = []
+	n_predict_addresses = []
+	n_learn_addresses   = []
+
+	# Determine neurons' active state
 	for ac in c_active_addresses:
 		pattern_recognized = False
+		n_learn_chosen = False
 
-#		for n in range(num_neurons):
-#			if n_active_addresses[ac, n] in predict_addresses[ac, n]:
-#				pattern_recognized = True
-#				n_active_addresses[ac, n] = 
+		for n in range(num_neurons):
+			if [ac, n] in n_previous_predict_addresses:
+				pattern_recognized = True
+				n_active_addresses.append([ac, n])
+				
 
 		if pattern_recognized == False:
 			for n in range(num_neurons):
 				n_active_addresses.append([ac, n])
 
-		print(n_active_addresses)				
+		if n_learn_chosen == False:
+			n_learn = 0
+			n_learn_addresses.append([ac, n_learn])
+#			print(bs_addresses[ac][n_learn])
+#			print(n_previous_active_addresses)
+#			bs_addresses[ac][n_learn].append(n_previous_active_addresses)
+#			bs_permanences
 
-		return n_active_addresses
+#	print(np.array(bs_addresses))
+
+	# Determine neurons' predict state
+#	for c in range(num_columns):
+#		for n in range (num_neurons):
+#			if
+#				n_predict_addresses.append([c, n])
 
 
+	# Learning
+
+	return n_active_addresses
+
+
+"""CLEAN THIS"""
 """MAKE MORE EFFICIENT"""
-def adaptSynapses(axons, s_addresses, s_permanences, s_threshold):
+def adaptSynapses(axons, d_s_addresses, d_s_permanences, s_threshold):
 	s_learning_rate = 1
 	s_permanence_lower = 0
 	s_permanence_upper = 99
 
-	for s in range(len(s_addresses)):
+	for d in range(len(d_s_addresses)):
+		for s in range(len(d_s_addresses[0])):
 
-		# Increment synapse permanence if connected axon is active or permanence is above threshold
-		if axons[s_addresses[s]] == 1 and s_permanences[s] < s_permanence_upper:
-			s_permanences[s] += s_learning_rate
+			# Increment a synapse permanence in a dendrite segment if connected axon is active or permanence is above threshold
+			if axons[d_s_addresses[d][s]] == 1 and d_s_permanences[d][s] < s_permanence_upper:
+				d_s_permanences[d][s] += s_learning_rate
 
-		# Decrement synapse permanence if connected axon is inactive or permanence is below threshold
-		elif axons[s_addresses[s]] == 0 or s_permanences[s] > s_permanence_lower:
-			s_permanences[s] -= s_learning_rate
+			# Decrement a synapse permanence in a dendrite segment if connected axon is inactive or permanence is below threshold
+			elif axons[d_s_addresses[d][s]] == 0 or d_s_permanences[d][s] > s_permanence_lower:
+				d_s_permanences[d][s] -= s_learning_rate
 
-			#  if synapse permanence falls below 0, randomly connect to new axon
-			if s_permanences[s] < 0:
-				connections = np.zeros(len(axons), dtype=np.int8)
-				connections[s_addresses] = 1
-				unused_connections = np.logical_not(connections)
-				new_addresses = np.nonzero(unused_connections > 0)[0] 
+				#  if synapse permanence falls below 0, randomly connect to new axon
+				if d_s_permanences[d][s] < 0:
+					connections = np.zeros(len(axons), dtype=np.int8)
+					connections[d_s_addresses] = 1
+					unused_connections = np.logical_not(connections)
+					new_addresses = np.nonzero(unused_connections > 0)[0] 
 
-				s_addresses[s]   = np.random.choice(new_addresses, None, replace=False)
-				s_permanences[s] = s_threshold + 1
+					d_s_addresses[d][s]   = np.random.choice(new_addresses, None, replace=False)
+					d_s_permanences[d][s] = s_threshold + 1
